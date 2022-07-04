e8c61c182dc308b786a00e3ff5a4d7baab24ab11
swedax94 <niclas-stoehr@t-online.de>
2022-07-04 19:38:46 +0200

    No Google | using image-charts to get SVG and PNG
    Added image-charts to get SVG and PNG Download.
    Simply added this in Custom CSS too because of the new ID:
    
    #qrcode2, #qrcode3 {
    	display: flex;
    	display: flex;
    	justify-content: center;
    	align-items: center;
    	flex-direction: column;
    }
    
    But I can't fix my Share problem on Main-Adminpage with share button. The QR-Codes don't appear right because of Ajax!?. 
    It seems to be loading in the wrong order, but I can't find the hook. The QR-Codes can correctly be downloaded at the Statistic Button Share Box. At the moment, I fixed it temporary with hiding the QR via CSS on Main-Adminpage:
    
    .index.desktop #qrcode2, .index.desktop #qrcode3 {
    	display: none !important;
    }

--- inline-qrcode.js
+++ inline-qrcode.js
@@ -1,8 +1,9 @@
 function qrcode_code() {
   var url = $( '#copylink' ).attr('value');
-  var qrcimg = 'http://chart.apis.google.com/chart?chs=150x150&cht=qr&chld=M&chl='+url;
-  var qrcimglnk = 'http://chart.apis.google.com/chart?chs=500x500&cht=qr&chld=M&chl='+url;	
-  var insertimg = "<div id='qrcode2' class='share'><a href="+qrcimglnk+"><img id='myid' src='" + qrcimg + "' /></a></div>"
+  var qrcimg = `https://image-charts.com/chart?chs=100x100&cht=qr&chld=M&chof=.png&chl=${url}`;
+  var qrcPNGlnk = `https://image-charts.com/chart?chs=500x500&cht=qr&chld=M&chof=.svg&chl=${url}`;
+  var qrcSVGlnk = `https://image-charts.com/chart?chs=500x500&cht=qr&chld=M&chof=.svg&chl=${url}`;	
+  var insertimg = `<div id='qrcode2' class='share'><a href="${qrcSVGlnk}" target='_blank'><img id='myid' src='${qrcimg}' /></a>SVG</div> <div id='qrcode3' class='share'><a href='${qrcPNGlnk}' target="_blank"><img id='myid' src='${qrcimg}' /></a>PNG</div>`;
 
   if ( $( '#qrcode2' ).length > 0 ) {
     $( '#qrcode2' ).remove( );
@@ -11,8 +12,10 @@ function qrcode_code() {
     $( "#shareboxes" ).append( insertimg );        // Append new elements
     $( "div#qrcode2 img" ).css( "width", "100px" );
     $( "div#qrcode2 img" ).css( "height", "100px" );
+    $( "div#qrcode3 img" ).css( "width", "100px" );
+    $( "div#qrcode3 img" ).css( "height", "100px" );
   }
 
 }
 
-$(document).ready( function(){ qrcode_code() } );
+$(document).ready( function(){qrcode_code() } );
